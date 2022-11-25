a3392751cc1329682a2776823256705b9baf25fc
dvolpato.gredom@gmail.com <dann.volpato@gmail.com>
2022-11-25 17:43:42 -0300

    Tratamento de httpstatus 429

--- src/main/java/br/com/gredom/webscraping/usecase/scraping/impl/ScrapingMagaluJson.java
+++ src/main/java/br/com/gredom/webscraping/usecase/scraping/impl/ScrapingMagaluJson.java
@@ -4,7 +4,9 @@ import br.com.gredom.webscraping.dto.ScrapingItemDto;
 import br.com.gredom.webscraping.enums.Company;
 import br.com.gredom.webscraping.response.JsonMagalu;
 import br.com.gredom.webscraping.response.ScrapingResponse;
+import br.com.gredom.webscraping.util.GerarArquivo;
 import br.com.gredom.webscraping.util.Strings;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +21,8 @@ public class ScrapingMagaluJson {
 
     public static final Company company = Company.MAGALU;
     private final JsonMagaluClient magaluClient;
+    private final GerarArquivo gerarArquivo;
+    private int sleepInSecs = 10;
 
     @Value("${application.scraping.magalu.homepage}")
     private String urlHomePage;
@@ -52,16 +56,17 @@ public class ScrapingMagaluJson {
             int page = 1;
             int pages = page;
             while (page <= pages) {
-                JsonMagalu dept = magaluClient.getByPath(path, page, splitedPath[0], splitedPath[2]);
+                JsonMagalu dept = getRespnseByPath(path, splitedPath[0], splitedPath[2], page);
                 qtRequest++;
 
                 int qtProdutos = dept.getPageProps().getData().getSearch().getProducts().size();
 
-                System.out.println(String.format("%s - page %s - found: %s", path, page, qtProdutos));
+                System.out.println(String.format("%s - page %s - itens: %s", path, page, qtProdutos));
 
                 pages = dept.getPageProps().getData().getSearch().getPagination().getPages();
-                if (qtRequest % poolRequests == 0) Thread.sleep(10_000);
-                Thread.sleep(2_000);
+
+//                if (qtRequest % poolRequests == 0) Thread.sleep(10_000);
+//                Thread.sleep(1_000);
 
                 List<ScrapingItemDto> products = dept.getPageProps().getData().getSearch().getProducts().stream()
                         .map(p -> ScrapingItemDto.build(company, "", "", p.getTitle(), p.getPrice().getPrice(), "", "", ""))
@@ -71,7 +76,37 @@ public class ScrapingMagaluJson {
                 page++;
             }
         }
-        System.out.println(qtRequest);
+
+        gerarArquivo.execute(response);
+
+        System.out.println(String.format("Requests: %s, itens: %s", qtRequest, response.getItens().size()));
+        System.out.println(String.format("Sleep max: %s seconds", sleepInSecs));
+
+        return response;
+    }
+
+    private JsonMagalu getRespnseByPath(String path, String path0, String path2, int page) throws Exception {
+
+        JsonMagalu response = new JsonMagalu();
+
+        boolean ok = false;
+        int qtStatus429 = 0;
+
+        while (!ok) {
+            try {
+                response = magaluClient.getByPath(path, page, path0, path2);
+                ok = true;
+            } catch (FeignException.TooManyRequests e) {
+                if (e.status() == 429) {
+                    System.out.println(String.format("sleep to %s seconds - %s", sleepInSecs, path));
+                    Thread.sleep(5 * 1_000);
+
+                    if (qtStatus429 > 0)
+                        sleepInSecs += 2;
+                    qtStatus429++;
+                }
+            }
+        }
 
         return response;
     }
